27428e4862b5204fa200335234e4ca3b89dd5709
angenue <angelica1nuesi@gmail.com>
2022-11-21 19:20:32 -0500

    code code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let result = '';
 
+    if (num < 0) {
+        return 'ERROR'
+    }
+    else if (string === ''){
+        return '';
+    }
+    else {
+        for (i = 0; i < num; i++) {
+            result += string;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
