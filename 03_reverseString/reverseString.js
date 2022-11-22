22bb849dee2f0152a0a736a545fd6e3e529b7d90
angenue <angelica1nuesi@gmail.com>
2022-11-21 19:23:02 -0500

    code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(array) {
+    const reverseArray = [];
+    
+    for (i = array.length-1; i >= 0 ;i--) {
+        reverseArray.push(array[i])
+    }
 
+    return reverseArray.join('');
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  var celsius = 5 * (num - 32) / 9;
+  var celsius = Math.round(celsius*10)/10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  var fahrenheit = ((num * 9) / 5) + 32;
+  var fahrenheit = Math.round(fahrenheit*10)/10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
